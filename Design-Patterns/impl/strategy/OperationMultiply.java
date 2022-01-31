2932222665af2364de229ebc3f5443529cb31c59
Jabed Bangali <jabedhasan21@yahoo.com>
2022-01-31 20:18:39 +0600

    impl: strategy design pattern

---
+++ Design-Patterns/impl/strategy/OperationMultiply.java
@@ -0,0 +1,9 @@
+public class OperationMultiply implements Strategy {
+
+    @Override
+    public int doOperation(int num1, int num2) {
+        // TODO Auto-generated method stub
+        return num1*num2;
+    }
+    
+}
